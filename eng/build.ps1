4ab78450ba365cbc32bd5d26c0e543f8b51ee696
Bill Menees <Bill@Menees.com>
2024-06-01 17:09:07 -0500

    Switch from msbuild to dotnet build and publish

--- eng/build.ps1
+++ eng/build.ps1
@@ -25,9 +25,7 @@ if ($build)
 {
 	foreach ($configuration in $configurations)
 	{
-		# Restore NuGet packages first
-		msbuild $slnPath /p:Configuration=$configuration /t:Restore /v:$msBuildVerbosity /nologo
-		msbuild $slnPath /p:Configuration=$configuration /v:$msBuildVerbosity /nologo
+		dotnet build $slnPath /p:Configuration=$configuration /v:$msBuildVerbosity /nologo
 	}
 }
 
@@ -60,7 +58,7 @@ if ($publish)
 					# The Publish target in "C:\Program Files\dotnet\sdk\3.1.101\Sdks\Microsoft.NET.Sdk\targets\Microsoft.NET.Sdk.CrossTargeting.targets"
 					# throws an exception if the .csproj uses <TargetFrameworks>. We have to override that and force a specific <TargetFramework> instead.
 					$targetFramework = GetXmlPropertyValue $profile 'TargetFramework'
-					msbuild $slnPath /t:Publish /p:PublishProfile=$profileName /p:TargetFramework=$targetFramework /v:$msBuildVerbosity /nologo /p:Configuration=$configuration
+					dotnet publish $slnPath /t:Publish /p:PublishProfile=$profileName /p:TargetFramework=$targetFramework /v:$msBuildVerbosity /nologo /p:Configuration=$configuration
 
 					Remove-Item "$artifactsPath\$profileName\*.pdb"
 
